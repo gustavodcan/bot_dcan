c964c0f5d1c478181ce9c79ae6d9f2190e676300
gustavodcan <desenvolvimento@dcan.com.br>
2025-07-22 16:55:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -538,7 +538,7 @@ def enviar_lista_setor(numero, mensagem):
 # Redireciona mensagem digitada para número do setor
 def encaminhar_para_setor(numero_usuario, setor, mensagem):
     mapa_setores = {
-        "comercial": "5511912538457",
+        "comercial": "5511969098799",
         "faturamento": "5511912538457",
         "financeiro": "5511912538457",
         "rh": "5511912538457"
@@ -568,7 +568,7 @@ def tratar_descricao_setor(numero, texto_recebido):
     if setor:
         encaminhar_para_setor(numero_usuario=numero, setor=setor, mensagem=texto_recebido)
         enviar_mensagem(numero, f"📨 Sua mensagem foi encaminhada ao setor {setor.title()}. Em breve alguém entrará em contato.")
-        conversas.pop(numero)
+        conversas.pop(numero, None)
     else:
         enviar_mensagem(numero, "⚠️ Setor não identificado. Vamos começar novamente.")
         enviar_lista_setor(numero)
