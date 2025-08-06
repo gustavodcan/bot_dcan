35a1b254e16543adf3b3325ae11ead86b45ff418
gustavodcan <desenvolvimento@dcan.com.br>
2025-08-06 10:59:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,13 @@ from google.oauth2.service_account import Credentials
 from integracoes.azure import salvar_imagem_azure
 from integracoes.infosimples import consultar_nfe_completa
 from operacao.foto_ticket.defs import extrair_dados_da_imagem
-from operacao.foto_ticket.saae import extrair_dados_cliente_saae
 from operacao.foto_ticket.orizon import extrair_dados_cliente_orizon
-from operacao.foto_ticket.estados import processar_confirmacao_final
-from operacao.foto_nf.estados import tratar_estado_aguardando_imagem_nf
-from operacao.foto_ticket.estados import tratar_estado_aguardando_imagem
-from operacao.foto_ticket.saae import tratar_estado_aguardando_destino_saae
-from operacao.foto_ticket.estados import tratar_estado_aguardando_confirmacao
-from operacao.foto_ticket.estados import tratar_estado_aguardando_nota_manual
-from operacao.foto_nf.estados import tratar_estado_aguardando_confirmacao_chave
 from integracoes.google_vision import (ler_texto_google_ocr, preprocessar_imagem)
 from operacao.falar_programador.contato import encaminhar_para_setor, tratar_descricao_setor
+from operacao.foto_ticket.saae import tratar_estado_aguardando_destino_saae, extrair_dados_cliente_saae
 from mensagens import (enviar_mensagem, enviar_botoes_sim_nao, enviar_lista_setor, enviar_opcoes_operacao)
+from operacao.foto_nf.estados import tratar_estado_aguardando_confirmacao_chave, tratar_estado_aguardando_imagem_nf
+from operacao.foto_ticket.estados import tratar_estado_aguardando_confirmacao, tratar_estado_aguardando_nota_manual, tratar_estado_aguardando_imagem, processar_confirmacao_final
 from config import (AZURE_FILE_ACCOUNT_NAME, AZURE_FILE_ACCOUNT_KEY, AZURE_FILE_SHARE_NAME, CERTIFICADO_BASE64, CERTIFICADO_SENHA, INFOSIMPLES_TOKEN, CHAVE_AES, GOOGLE_SHEETS_PATH, GOOGLE_CREDS_PATH, GOOGLE_CREDS_JSON, INSTANCE_ID, API_TOKEN, CLIENT_TOKEN)
 
 app = Flask(__name__)
