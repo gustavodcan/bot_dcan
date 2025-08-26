cfe43fa981d5e7f4c9171b1b24720343c57b610e
gustavodcan <desenvolvimento@dcan.com.br>
2025-08-26 11:09:26 -0300

    Update supabase_db.py

--- integracoes/supabase_db.py
+++ integracoes/supabase_db.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 SUPABASE_URL = os.getenv("SUPABASE_DB_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY") or os.getenv("SUPABASE_ANON_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_DB_URL, SUPABASE_KEY)
 
 def salvar_viagem(dados: dict):
     """
