8bf3eec01866bbd0f7a952f3b22eedba5156233a
gustavodcan <desenvolvimento@dcan.com.br>
2025-09-29 12:47:39 -0300

    Update checklist.py

--- manutencao/checklist.py
+++ manutencao/checklist.py
@@ -2,3 +2,9 @@ import logging, os, requests
 from mensagens import enviar_mensagem
 
 logger = logging.getLogger(__name__)
+
+if estado == "aguardando_placa_manutencao":
+  enviar_mensagem(numero, f"✏️ Por favor, envie a placa do veículo.")
+  else:
+    enviar_lista_setor(numero, "❌ Opção inválida. Por favor, escolha uma opção da lista.")
+  return jsonify(status="resposta motorista")
