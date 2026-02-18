f741919b6c123f4cab22511fa0aac39c5de3b552
gustavodcan <desenvolvimento@dcan.com.br>
2026-02-18 12:35:55 -0300

    Update gerdau.py

--- operacao/foto_ticket/gerdau.py
+++ operacao/foto_ticket/gerdau.py
@@ -13,7 +13,7 @@ def extrair_dados_cliente_gerdau(img, texto: str):
     logger.debug(texto)
 
     # Gerdau Geral: ticket com 8 dígitos
-    m_ticket_geral = re.search(r"(^\s*\d{8,9}\s*$)", texto, re.MULTILINE)
+    m_ticket_geral = re.search(r"^.*?(?<!\d)(\d{8,9})(?!\d).*$", texto, re.MULTILINE)
     ticket_val = m_ticket_geral.group(1) if m_ticket_geral else NAO_ENCONTRADO
 
     # Gerdau Pinda: “processo: 74928/1” (5+ dígitos com possível “/”)
