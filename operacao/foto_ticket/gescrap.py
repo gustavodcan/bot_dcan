590fe30cb040354a73d522f4eed2f591b2961fb9
gustavodcan <desenvolvimento@dcan.com.br>
2026-03-03 17:40:57 -0300

    Update gescrap.py
    placeholder Gescrap

--- operacao/foto_ticket/gescrap.py
+++ operacao/foto_ticket/gescrap.py
@@ -1,1 +1,46 @@
+# ===== Standard library =====
+import re
+import logging
 
+logger = logging.getLogger(__name__)
+
+def extrair_dados_cliente_gescrap(img, texto):
+    logger.debug("📜 Cliente Detectado: Gescrap")
+    logger.debug(texto)
+    
+    # Ticket
+    m_ticket_ectx = re.search(r"(?i)\bboleto\b[\s:]*([0-9/]{3,})", texto)
+    ticket_val_ectx = m_ticket_ectx.group(1) if m_ticket_ectx else NAO_ENCONTRADO
+    
+    # Peso
+    
+    # Se achar 6 match's, selecionar a 5, se achar 5 match's selecionar a 5.
+    matches_validos = []
+    
+    matches = re.findall(r"\b(\d{1,3}\s*[.,]\s*\d{3})\b", texto)
+
+    matches_validos = [
+        m.replace(",", ".").replace(" ", "")
+        for m in matches
+    ]
+    
+    peso_liquido = None
+
+    logger.debug(matches_validos)
+
+    if len(matches_validos) == 6:
+        peso_liquido = matches_validos[4]
+
+    elif len(matches_validos) == 5:
+        peso_liquido = matches_validos[4]
+
+    logger.debug("🎯 Dados extraídos:")
+    logger.debug(f"Nota Fiscal: {ticket_val_ectx}")
+    logger.debug(f"Ticket: {ticket_val_ectx}")
+    logger.debug(f"Peso Líquido: {peso_liquido}")
+
+    return {
+        "nota_fiscal": ticket_val_ectx,
+        "ticket": ticket_val_ectx,
+        "peso_liquido": peso_liquido
+    }
