78d86c2beb53259f4772a5da3d01dc5649c37c53
gustavodcan <desenvolvimento@dcan.com.br>
2026-03-03 10:38:13 -0300

    Update eucatex.py

--- operacao/foto_ticket/eucatex.py
+++ operacao/foto_ticket/eucatex.py
@@ -1,1 +1,32 @@
+# ===== Standard library =====
+import re
+import logging
 
+logger = logging.getLogger(__name__)
+
+def extrair_dados_cliente_ternium(img, texto):
+    logger.debug("📜 Cliente Detectado: Ternium")
+    logger.debug(texto)
+
+    # Nota
+    nf_match = re.search(r"\bitem:\s*(?:\[?n\]?\s*)?(\d+)-", texto, re.IGNORECASE)
+    nota_val = nf_match.group(1) if nf_match else None
+    
+    # Ticket
+    ticket_match = re.search(r"\bpesagem:\s*(\d+)", texto, re.IGNORECASE)
+    ticket_val = ticket_match.group(1) if ticket_match else None
+
+    # Peso
+    peso = re.search(r"\bl[íi]qu[ií]do:\s*(\d+)", texto, re.IGNORECASE)
+    peso_liquido = peso.group(1) if peso else None
+
+    logger.debug("🎯 Dados extraídos:")
+    logger.debug(f"Nota Fiscal: {nota_val}")
+    logger.debug(f"Ticket: {ticket_val}")
+    logger.debug(f"Peso Líquido: {peso_liquido}")
+
+    return {
+        "nota_fiscal": nota_val,
+        "ticket": ticket_val,
+        "peso_liquido": peso_liquido
+    }
